e189d421320f1bdabf48262d2e45540563df402b
Anushka Chandel <51352909+ChandelAnushka@users.noreply.github.com>
2025-09-21 20:23:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ async function startMic() {
     function checkBlow() {
       analyser.getByteFrequencyData(data);
       let volume = data.reduce((a, b) => a + b) / data.length;
-      if (volume > 12) {
+      if (volume > 10) {
         candles.forEach(c => (c.lit = false));
         render();
         updateCandleCount();
